634bde90a6418c3d89ee0e65e7360fb230f498d3
Genesis <kgashwanetshimologo@gmail.com>
2024-09-14 18:21:28 +0200

    updated javascript

--- javascropt/index.js
+++ javascropt/index.js
@@ -10,7 +10,7 @@ let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY")
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]AM[</small>]");
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
 
 }
 
