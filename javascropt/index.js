d4c099bf6bf7729507e2f704f957a5ca2d3af765
Genesis <kgashwanetshimologo@gmail.com>
2024-09-16 09:02:15 +0200

    updated html file

--- javascropt/index.js
+++ javascropt/index.js
@@ -1,20 +1,17 @@
-function updateTime(){
+function updateTime() {
+  
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-
-
-let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement){
-
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
-
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY")
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
-
-}
-
-let parisElement = document.querySelector("#paris");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  let parisElement = document.querySelector("#paris");
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
     let parisTimeElement = parisElement.querySelector(".time");
@@ -45,6 +42,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <a href="/">All cities</a>
   `;
 }
 
